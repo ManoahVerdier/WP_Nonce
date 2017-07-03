28df371b3451d4f29f1373afb0ab659ff04ba8da
ManoahVerdier <verdier.developpement@gmail.com>
2017-07-03 17:22:16 +0200

    Update wp_nonce.php

--- wp_nonce.php
+++ wp_nonce.php
@@ -1,6 +1,28 @@
 ```php
-
+<?php
 class wp_nonce{
+  public static $time_keepalive;
+  
+  public $name;
+  public $url;
+  public $action;
+  
+  public function __contruct($action = $name = ""){
+    
+  }
+  
+  public function __destroy(){
+    
+  }
+  /*
+  public function getTimeKeepAlive(){
+    return $this->time_keepalive;
+  }
+  
+  public function setTimeKeepAlive($new_time){
+    $this->time_keepalive;
+  }
+  */
   
 }
 
